286fd92a44160ac62c3e47d10e62b02efed430b7
Hyungsuk Kang <hskang9@github.com>
2022-02-25 17:25:19 +0900

    remove dist

--- dist/helper/transactions.d.ts
+++
@@ -1,4 +0,0 @@
-import { ethers } from "ethers";
-export declare function executeTx(tx: any, event: string): Promise<any>;
-export declare function deployContract(deploy: ethers.Contract, contract: string): Promise<void>;
-export declare function executeFrom(ethers: any, deployer: any, func: any): Promise<void>;
--- dist/image.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function getDataURI(nft: any, chainId: any, tokenId: any): Promise<string>;
-export declare function imgToDataURI(path: any): Promise<any>;
